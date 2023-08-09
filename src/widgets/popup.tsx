1d1d5cee6435ea54229116a305634d1307255f82
voidtriangle <107706537+voidtriangle@users.noreply.github.com>
2023-08-09 19:45:20 +0530

    Add files via upload

--- src/widgets/popup.tsx
+++ src/widgets/popup.tsx
@@ -1,25 +1,27 @@
-import { AppEvents, Queue, renderWidget,useSessionStorageState } from "@remnote/plugin-sdk";
+import React from 'react';
+import { AppEvents, Queue, renderWidget, useSessionStorageState } from "@remnote/plugin-sdk";
+
+function Statistic({ label, value, id }) {
+  return (
+    <div id={id} style={{ margin: '0 20px' }}>
+      {label}: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{value}</span>
+    </div>
+  );
+}
 
 function popup() {
   const [cardPerMinute] = useSessionStorageState("cardPerMinute", 0);
   const [remainingTime] = useSessionStorageState("remainingTime", 0);
   const [totalCardsCompleted] = useSessionStorageState("totalCardsCompleted", 0);
-
-  
+  const [totalTimeSpent] = useSessionStorageState("totalTimeSpent", 0);
 
   return (
     <div id="card-stats" style={{ display: 'flex', justifyContent: 'space-between', whiteSpace: 'nowrap', overflowX: 'auto', fontFamily: 'Bookerly, "Segoe UI", sans-serif' }}>
-  <div id="cards-per-minute" style={{ margin: '0 20px' }}>
-    Cards per minute: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{cardPerMinute}</span>
-  </div>
-  <div id="time-to-finish" style={{ margin: '0 20px' }}>
-    Time to Finish: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{remainingTime}</span>
-  </div>
-  <div id="cards-reviewed" style={{ margin: '0 20px' }}>
-    Cards Reviewed: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{totalCardsCompleted}</span>
-  </div>
-</div>
-
+      <Statistic label="Cards per minute" value={cardPerMinute} id="cards-per-minute" />
+      <Statistic label="Time to Finish" value={remainingTime} id="time-to-finish" />
+      <Statistic label="Cards Reviewed" value={totalCardsCompleted} id="cards-reviewed" />
+      <Statistic label="Time Spent" value={`${totalTimeSpent} Minute`} id="time-spent" />
+    </div>
   );
 }
 
