91f9a4ff311cf6e20bb273fe6ac759de46b9158d
voidtriangle <107706537+voidtriangle@users.noreply.github.com>
2023-08-10 03:10:30 +0530

    Add files via upload

--- src/widgets/popup.tsx
+++ src/widgets/popup.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
-import { AppEvents, Queue, renderWidget, useSessionStorageState } from "@remnote/plugin-sdk";
-
-function Statistic({ label, value, id }) {
-  return (
-    <div id={id} style={{ margin: '0 20px' }}>
-      {label}: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{value}</span>
-    </div>
-  );
-}
+import { AppEvents, Queue, renderWidget,useSessionStorageState } from "@remnote/plugin-sdk";
 
 function popup() {
   const [cardPerMinute] = useSessionStorageState("cardPerMinute", 0);
@@ -15,13 +6,24 @@ function popup() {
   const [totalCardsCompleted] = useSessionStorageState("totalCardsCompleted", 0);
   const [totalTimeSpent] = useSessionStorageState("totalTimeSpent", 0);
 
+  
+
   return (
     <div id="card-stats" style={{ display: 'flex', justifyContent: 'space-between', whiteSpace: 'nowrap', overflowX: 'auto', fontFamily: 'Bookerly, "Segoe UI", sans-serif' }}>
-      <Statistic label="Cards per minute" value={cardPerMinute} id="cards-per-minute" />
-      <Statistic label="Time to Finish" value={remainingTime} id="time-to-finish" />
-      <Statistic label="Cards Reviewed" value={totalCardsCompleted} id="cards-reviewed" />
-      <Statistic label="Time Spent" value={`${totalTimeSpent} Minute`} id="time-spent" />
-    </div>
+  <div id="cards-per-minute" style={{ margin: '0 20px' }}>
+    Cards per minute: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{cardPerMinute}</span>
+  </div>
+  <div id="time-to-finish" style={{ margin: '0 20px' }}>
+    Time to Finish: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{remainingTime}</span>
+  </div>
+  <div id="cards-reviewed" style={{ margin: '0 20px' }}>
+    Cards Reviewed: <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{totalCardsCompleted}</span>
+  </div>
+   <div id="time-spent" style={{ margin: '0 10px' }}>
+    Time Spent : <span style={{ fontWeight: 'bold', color: 'lightgreen' }}>{totalTimeSpent} Minute</span>
+  </div>
+</div>
+
   );
 }
 
